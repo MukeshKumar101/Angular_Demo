bb61564dbb308585e3fabb60b2692c4a78fce1a2
MukeshKumar101 <maxw2764@gmail.com>
2020-09-22 18:40:30 +0530

    practiced on components and routing

--- angular-demo-app/src/app/app.module.ts
+++ angular-demo-app/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    ServicesComponent,
+    ContactComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
